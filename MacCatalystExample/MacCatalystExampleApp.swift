3a76fae8b9e08bb4664947e628bd5034dea2949d
devyhan <devyhan93@gmail.com>
2023-03-09 21:42:43 +0900

    Init Project

---
+++ MacCatalystExample/MacCatalystExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacCatalystExampleApp.swift
+//  MacCatalystExample
+//
+//  Created by YHAN on 2023/03/09.
+//
+
+import SwiftUI
+
+@main
+struct MacCatalystExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
