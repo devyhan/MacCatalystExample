4c3b6b3a796de243b69abd52c6c8146f6a2d78c9
YoHan Cho <devyhan93@gmail.com>
2023-03-16 13:50:55 +0900

    Added Dependencies Get, SkeletonView

--- MacCatalystExample/MacCatalystExampleApp.swift
+++ MacCatalystExample/MacCatalystExampleApp.swift
@@ -6,12 +6,21 @@
 //
 
 import SwiftUI
+import SkeletonView
 
 @main
 struct MacCatalystExampleApp: App {
     var body: some Scene {
         WindowGroup {
+          
+          
             ContentView()
         }
     }
+}
+
+
+public protocol SkeletonTableViewDelegate: UITableViewDelegate {
+    func collectionSkeletonView(_ skeletonView: UITableView, identifierForHeaderInSection section: Int) -> ReusableHeaderFooterIdentifier? // default: nil
+    func collectionSkeletonView(_ skeletonView: UITableView, identifierForFooterInSection section: Int) -> ReusableHeaderFooterIdentifier? // default: nil
 }
